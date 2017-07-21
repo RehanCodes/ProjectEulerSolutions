bc250ce57157c5882ef1e6263f03e79640e97735
Rehan <Rehan@sep.com>
2017-07-21 17:08:33 +0530

    Adding solution to problem 20 in dlModerate

---
+++ ProjectEulerSolutions/src/dlModerate/P20_FactorialDigitSum.java
@@ -0,0 +1,38 @@
+package dlModerate;
+
+import java.math.BigInteger;
+
+/* *
+ * Solution for Problem 20
+ * Factorial Digit Sum
+ * link: https://projecteuler.net/problem=20
+ * 
+ * @author: Rehan
+ * */
+
+public class P20_FactorialDigitSum {
+	
+	static BigInteger b=new BigInteger("1");
+	
+	static long mulplicationFactor=1l;
+	static int sum=0;
+
+	public static void main(String[] args) 
+	{
+		for(int i=100;i>=1;i--)
+		{
+			b=b.multiply(BigInteger.valueOf(i));
+		}
+		System.out.println("100! = "+b);
+		String str=String.valueOf(b);
+		System.out.println("Number of digits in 100! = "+str.length());
+		char arr[]=str.toCharArray();
+		for (char c : arr) {
+			String str2=String.valueOf(c);
+			int temp=Integer.parseInt(str2);
+			sum=sum+temp;
+		}
+		System.out.println("Sum of the digits of 100! = "+sum);
+	}
+}
+// Kindly suggest a way, if possible, to convert a char directly to an integer.
