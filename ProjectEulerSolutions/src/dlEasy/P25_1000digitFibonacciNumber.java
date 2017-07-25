b56273c5ac7801593730340c1d01ef7d25d4918d
Rehan <Rehan@sep.com>
2017-07-25 15:51:20 +0530

    Adding solution of Problem 25 via Branch Problem25

---
+++ ProjectEulerSolutions/src/dlEasy/P25_1000digitFibonacciNumber.java
@@ -0,0 +1,38 @@
+package dlEasy;
+
+/* *
+ * Solution for Problem 25
+ * 1000 digit Fibonacci Number
+ * link: https://projecteuler.net/problem=25
+ * 
+ * @author: Rehan
+ * */
+
+import java.math.BigInteger;
+
+public class P25_1000digitFibonacciNumber {
+
+	public static void main(String[] args) 
+	{
+		BigInteger i=BigInteger.ZERO;
+		BigInteger j=BigInteger.ONE;
+		BigInteger sum=BigInteger.ZERO;
+		int counter=1;
+		
+		System.out.println(j);
+		for(int k=1;k<=5000;k++)
+		{
+			sum=i.add(j);
+			counter++;
+			System.out.println(sum);
+			
+			if(String.valueOf(sum).length()==1000)
+			{
+				System.out.println("FOUND!! the 1000 digit Fibonacci number = "+sum+" | At INDEX : "+counter);
+				break;
+			}
+			i=j;
+			j=sum;
+		}
+	}
+}
