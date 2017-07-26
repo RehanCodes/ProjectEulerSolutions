9b0b664217a5c1a6a58140a0993f89209079cd2c
Rehan <Rehan@sep.com>
2017-07-26 12:24:37 +0530

    Adding the Util Package which has the Runner Class

---
+++ ProjectEulerSolutions/src/util/Runner.java
@@ -0,0 +1,35 @@
+package util;
+
+import dlEasy.P13_LargeSum;
+import dlEasy.P1_Multiples3n5;
+import dlEasy.P25_1000digitFibonacciNumber;
+import dlEasy.P2_EvenFibonacciNumbers;
+import dlEasy.P3_LargestPrimeFactor;
+import dlEasy.P6_SumSquareDifference;
+import dlEasy.P9_SpecialPythagoreanTriplet;
+
+public class Runner {
+
+	public static void main(String[] args) 
+	{
+		dlEasy.P1_Multiples3n5.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlEasy.P2_EvenFibonacciNumbers.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlEasy.P3_LargestPrimeFactor.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlEasy.P6_SumSquareDifference.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlEasy.P9_SpecialPythagoreanTriplet.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlEasy.P13_LargeSum.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlModerate.P20_FactorialDigitSum.main(null);
+		System.out.println("**********************************************************************************************************");
+		dlEasy.P25_1000digitFibonacciNumber.main(null);
+		System.out.println("**********************************************************************************************************");
+		
+		
+	}
+
+}
