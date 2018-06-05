f2a72924362ddc4b32ca8c81bc8b2fa76efb4c50
Rehan <Rehan@sep.com>
2018-06-05 18:14:03 +0530

    Commiting the tesmporary test class

---
+++ ProjectEulerSolutions/src/solutions/TempTestClass.java
@@ -0,0 +1,10 @@
+package solutions;
+
+public class TempTestClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test class to check the git commands !");
+
+	}
+
+}
